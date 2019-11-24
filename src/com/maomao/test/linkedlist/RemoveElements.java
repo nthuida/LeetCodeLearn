1cf4537cdf1719d71c40f62a2827d4a808fde0a7
maohuida <maohuida2009@126.com>
2019-11-24 19:48:21 +0800

    代码整理

--- src/com/maomao/test/linkedlist/RemoveElements.java
+++ src/com/maomao/test/linkedlist/RemoveElements.java
@@ -2,17 +2,17 @@ package com.maomao.test.linkedlist;
 
 /**
  * 移除链表元素
+ *
+ * 删除链表中等于给定值 val 的所有节点。
+ * 示例:
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ *
  * @Author huida.mao
  * @Date 2019/10/24
  */
 public class RemoveElements {
     /**
-     * 删除链表中等于给定值 val 的所有节点。
-     *
-     * 示例:
-     *
-     * 输入: 1->2->6->3->4->5->6, val = 6
-     * 输出: 1->2->3->4->5
      * @param head
      * @param val
      * @return
@@ -22,6 +22,7 @@ public class RemoveElements {
             return null;
         }
         ListNode node = head;
+        //新建链表存放结果
         ListNode result = new ListNode(1);
         ListNode a = result;
         while (node != null) {
