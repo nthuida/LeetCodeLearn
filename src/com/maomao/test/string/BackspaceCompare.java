cd6bb3b35b78dac75fa417dd8855e482e12c5e39
huida.mao <huida.mao@onesmart.org>
2019-11-25 14:29:19 +0800

    代码整理

--- src/com/maomao/test/string/BackspaceCompare.java
+++ src/com/maomao/test/string/BackspaceCompare.java
@@ -3,18 +3,17 @@ package com.maomao.test.string;
 import java.util.Stack;
 
 /**
+ *比较含退格的字符串
+ *给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+ * 示例 1：
+ * 输入：S = "ab#c", T = "ad#c"
+ * 输出：true
+ * 解释：S 和 T 都会变成 “ac”。
  * @author Administrator
  * @date 2019/3/17
  */
 public class BackspaceCompare {
     /**
-     *比较含退格的字符串
-     *给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
-     * 示例 1：
-     * 输入：S = "ab#c", T = "ad#c"
-     * 输出：true
-     * 解释：S 和 T 都会变成 “ac”。
-     *
      * 用栈来保存两个字符串，并比较
      * @param S
      * @param T
--- src/com/maomao/test/string/LongestCommonPrefix.java
+++ src/com/maomao/test/string/LongestCommonPrefix.java
@@ -1,25 +1,24 @@
 package com.maomao.test.string;
 
 /**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ *
+ * 示例 2:
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
  * @Author huida.mao
  * @Date 2019/10/14
  */
 public class LongestCommonPrefix {
     /**
-     * 编写一个函数来查找字符串数组中的最长公共前缀。
-     * 如果不存在公共前缀，返回空字符串 ""。
-     *
-     * 示例 1:
-     * 输入: ["flower","flow","flight"]
-     * 输出: "fl"
-     *
-     * 示例 2:
-     * 输入: ["dog","racecar","car"]
-     * 输出: ""
-     * 解释: 输入不存在公共前缀。
-     *
      * 首先假设最长前缀result=str[0], 遍历字符串数组，依次与result做比较，
-     * 	 * 找出其最长前缀，然后更新result，再进行下一次比较。
+     * 找出其最长前缀，然后更新result，再进行下一次比较。
      * @param strs
      * @return
      */
