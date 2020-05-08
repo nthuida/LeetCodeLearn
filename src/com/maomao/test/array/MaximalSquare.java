e977136c7870835135515d2fb11fc20227ff842a
maohuida <maohuida@kingsoft.com>
2020-05-08 17:07:56 +0800

    5-8

---
+++ src/com/maomao/test/array/MaximalSquare.java
@@ -0,0 +1,55 @@
+package com.maomao.test.array;
+
+/**
+ * 最大正方形
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ *
+ * 示例:
+ * 输入:
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ *
+ * 输出: 4
+ *
+ * @author Administrator
+ * @date 2020/5/8
+ */
+public class MaximalSquare {
+
+    /**
+     * 动态规划
+     * 用 dp(i, j)表示以 (i, j)为右下角，且只包含 1的正方形的边长最大值。
+     * 则 dp(i, j) 的值由其上方、左方和左上方的三个相邻位置的 dp 值决定。
+     * 具体而言，当前位置的元素值等于三个相邻位置的元素中的最小值加 1，状态转移方程如下：
+     * dp(i, j)=min(dp(i−1, j), dp(i−1, j−1), dp(i, j−1))+1
+     *
+     * @param matrix
+     * @return
+     */
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        //边界为1
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}
---
+++ src/com/maomao/test/string/MyAtoi.java
@@ -0,0 +1,78 @@
+package com.maomao.test.string;
+
+/**
+ * 字符串转整数
+ *
+ * 示例 1:
+ * 输入: "42"
+ * 输出: 42
+ *
+ * 示例 2:
+ * 输入: "   -42"
+ * 输出: -42
+ * 解释: 第一个非空白字符为 '-', 它是一个负号。
+ *      我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。
+ *
+ * 示例 3:
+ * 输入: "4193 with words"
+ * 输出: 4193
+ * 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
+ *
+ * 示例 4:
+ * 输入: "words and 987"
+ * 输出: 0
+ * 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。
+ *      因此无法执行有效的转换。
+ *
+ * 示例 5:
+ * 输入: "-91283472332"
+ * 输出: -2147483648
+ * 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。
+ *      因此返回 INT_MIN (−231) 。
+ *
+ *  @author Administrator
+ *  @date 2020/5/8
+ */
+public class MyAtoi {
+
+    public int myAtoi(String str) {
+        if (str == null || str == "") {
+            return 0;
+        }
+        char[] chars = str.toCharArray();
+        int n = chars.length;
+        int idx = 0;
+        while (idx < n && chars[idx] == ' ') {
+            // 去掉前导空格
+            idx++;
+        }
+        if (idx == n) {
+            //去掉前导空格以后到了末尾了
+            return 0;
+        }
+        boolean negative = false;
+        if (chars[idx] == '-') {
+            //遇到负号
+            negative = true;
+            idx++;
+        } else if (chars[idx] == '+') {
+            // 遇到正号
+            idx++;
+        } else if (!Character.isDigit(chars[idx])) {
+            // 其他符号
+            return 0;
+        }
+        int ans = 0;
+        while (idx < n && Character.isDigit(chars[idx])) {
+            int digit = chars[idx] - '0';
+            if (ans > (Integer.MAX_VALUE - digit) / 10) {
+                // 本来应该是 ans * 10 + digit > Integer.MAX_VALUE
+                // 但是 *10 和 + digit 都有可能越界，所有都移动到右边去就可以了。
+                return negative? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            ans = ans * 10 + digit;
+            idx++;
+        }
+        return negative? -ans : ans;
+    }
+}
