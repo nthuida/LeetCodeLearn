3fdaa36cc3357a844541141bb9d56569b6c049b1
maohuida <maohuida@kingsoft.com>
2020-05-11 14:30:34 +0800

    5-11

--- src/com/maomao/test/array/ThreeSum.java
+++ src/com/maomao/test/array/ThreeSum.java
@@ -21,6 +21,7 @@ public class ThreeSum {
     /**
      * 解题思路：先对数组排序，然后开始遍历，对于数组中的每一个元素，用两指针往中间夹，
      * 直到找出所有的解。时间复杂度 O(n^2).
+     * 超时
      * @param nums
      * @return
      */
@@ -28,6 +29,10 @@ public class ThreeSum {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+                break;
+            }
             int left = i+1;
             int right = nums.length-1;
             while (left < right) {
@@ -40,20 +45,65 @@ public class ThreeSum {
                     if (!result.contains(res)) {
                         result.add(res);
                     }
-                }
-                //找到，也要+1继续往前找
-                if (sum <= 0) {
+                    //找到，也要+1继续往前找
+                    left++;
+                    right--;
+                } else if (sum < 0) {
                     left++;
                 } else {
-                   right--;
+                    right--;
                 }
             }
         }
         return result;
     }
 
+    /**
+     * 主要优化重复的情况
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum1(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int k = 0; k < nums.length - 2; k++){
+            if(nums[k] > 0) {
+                // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+                break;
+            }
+            if(k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            int left = k+1;
+            int right = nums.length-1;
+            while(left < right){
+                int sum = nums[k] + nums[left] + nums[right];
+                if(sum < 0){
+                    //去重
+                    while(left < right && nums[left] == nums[++left]) {
+                        continue;
+                    }
+                } else if (sum > 0) {
+                    while(left < right && nums[right] == nums[--right]) {
+                        continue;
+                    }
+                } else {
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k], nums[left], nums[right])));
+                    //去重
+                    while(left < right && nums[left] == nums[++left]) {
+                        continue;
+                    }
+                    while(left < right && nums[right] == nums[--right]) {
+                        continue;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[] nums = {-2,0,1,1,2};
         List<List<Integer>> result = new ThreeSum().threeSum(nums);
         for (List<Integer> list : result) {
             System.out.println(list);
--- src/com/maomao/test/tree/LowestCommonAncestor.java
+++ src/com/maomao/test/tree/LowestCommonAncestor.java
@@ -44,4 +44,29 @@ public class LowestCommonAncestor {
             return root;
         }
     }
+
+    /**
+     * 二叉树的最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q ) {
+            return root;
+        }
+        TreeNode leftCommonAncestor =  lowestCommonAncestor(root.left, p, q);
+        TreeNode rightCommonAncestor =  lowestCommonAncestor(root.right, p, q);
+        //在左子树中没有找到，那一定在右子树中
+        if(leftCommonAncestor == null){
+            return rightCommonAncestor;
+        }
+        //在右子树中没有找到，那一定在左子树中
+        if(rightCommonAncestor == null){
+            return leftCommonAncestor;
+        }
+        //不在左子树，也不在右子树，那说明是根节点
+        return root;
+    }
 }
