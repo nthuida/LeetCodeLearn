980d16e37675762467bbc5870859d21b32174673
maohuida <maohuida@kingsoft.com>
2020-05-11 10:37:07 +0800

    5-9

---
+++ src/com/maomao/test/array/MaxArea.java
@@ -0,0 +1,42 @@
+package com.maomao.test.array;
+
+/**
+ * 盛最多水的容器
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，
+ * 垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ *
+ * 示例：
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ *
+ * @author Administrator
+ * @date 2020/5/8
+ */
+public class MaxArea {
+
+    /**
+     * 循环比较
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j=i+1; j<height.length; j++) {
+                int x = j-i;
+                int y = Math.min(height[i],height[j]);
+                int area = x*y;
+                if (area > max) {
+                    max = area;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] area = {1,8,6,2,5,4,8,3,7};
+        System.out.println(new MaxArea().maxArea(area));
+    }
+}
