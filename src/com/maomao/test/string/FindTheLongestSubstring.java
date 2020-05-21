de9922ecaa0ce65e7e4ad66a66e7713adb9f11e8
maohuida <maohuida@kingsoft.com>
2020-05-21 11:06:56 +0800

    5-20

--- src/com/maomao/test/string/FindTheLongestSubstring.java
+++ src/com/maomao/test/string/FindTheLongestSubstring.java
@@ -78,4 +78,9 @@ public class FindTheLongestSubstring {
         }
         return ans;
     }
+
+    public static void main(String[] args) {
+        String s = "ell";
+        System.out.println(new FindTheLongestSubstring().findTheLongestSubstring(s));
+    }
 }
