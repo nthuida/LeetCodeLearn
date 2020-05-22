c1964a2fa07e23fc589b40204a98d89b2c268559
maohuida <maohuida@kingsoft.com>
2020-05-22 10:27:26 +0800

    5-21

--- src/com/maomao/test/array/Rotate.java
+++ src/com/maomao/test/array/Rotate.java
@@ -52,6 +52,93 @@ public class Rotate {
         }
     }
 
+    /**
+     *  旋转矩阵
+     *  给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+     *
+     *  示例 1:
+     * 给定 matrix =
+     * [
+     *   [1,2,3],
+     *   [4,5,6],
+     *   [7,8,9]
+     * ],
+     *
+     * 原地旋转输入矩阵，使其变为:
+     * [
+     *   [7,4,1],
+     *   [8,5,2],
+     *   [9,6,3]
+     * ]
+     *
+     * 示例 2:
+     * 给定 matrix =
+     * [
+     *   [ 5, 1, 9,11],
+     *   [ 2, 4, 8,10],
+     *   [13, 3, 6, 7],
+     *   [15,14,12,16]
+     * ],
+     *
+     * 原地旋转输入矩阵，使其变为:
+     * [
+     *   [15,13, 2, 5],
+     *   [14, 3, 4, 1],
+     *   [12, 6, 8, 9],
+     *   [16, 7,10,11]
+     * ]
+     *
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] rotateMatrix = new int[n][n];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                rotateMatrix[j][n-1-i] = matrix[i][j];
+            }
+        }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                matrix[i][j] = rotateMatrix[i][j];
+            }
+        }
+    }
+
+    /**
+     * 先由对角线 [1, 5, 9] 为轴进行翻转,于是数组变成了:
+     * [1,4,7]
+     * [2,5,8]
+     * [3,6,9]
+     *
+     * 再对每一行以中点进行翻转，就得到了
+     * [7,4,1]
+     * [8,5,2]
+     * [9,6,3]
+     * @param matrix
+     */
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+        // 先以对角线（左上-右下）为轴进行翻转
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // 再对每一行以中点进行翻转
+        int mid = n >> 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < mid; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7,8};
         new Rotate().rotate1(nums, 2);
---
+++ src/com/maomao/test/digit/WaysToChange.java
@@ -0,0 +1,80 @@
+package com.maomao.test.digit;
+
+/**
+ * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。
+ * (结果可能会很大，你需要将结果模上1000000007)
+ *
+ * 示例1:
+ *  输入: n = 5
+ *  输出：2
+ *  解释: 有两种方式可以凑成总金额:
+ * 5=5
+ * 5=1+1+1+1+1
+ *
+ * 示例2:
+ *  输入: n = 10
+ *  输出：4
+ *  解释: 有四种方式可以凑成总金额:
+ * 10=10
+ * 10=5+5
+ * 10=5+1+1+1+1+1
+ * 10=1+1+1+1+1+1+1+1+1+1
+ *
+ * @author huida
+ * @date 2020/5/21
+ */
+public class WaysToChange {
+
+    /**
+     * 完全背包问题
+     * @param n
+     * @return
+     */
+    public int waysToChange(int n) {
+        int[] coins = new int[]{1, 5, 10, 25};
+        // 一般多开一个位置，0 空着不用
+        int[][] dp = new int[5][n + 1];
+        // base case
+        for (int i = 1; i <= 4; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 下面这部分代码是可以进一步改写的，因为从状态转移方程里面可以看到都有 dp[i-1][j],
+                // 因此可以直接不用判断就赋值给 dp[i][j]，判断后再加上『 选择当前硬币时 』的补偿值就可以了
+
+                if (j - coins[i-1] < 0){
+                    // 要组成的面值比当前硬币金额小，该硬币不可以选择
+                    // 只能由 i - 1 中硬币来组成面值 j
+                    dp[i][j] = dp[i - 1][j] % 1000000007;
+                } else {
+                    // 当前硬币可以不选，也可以选择
+                    dp[i][j] = (dp[i - 1][j] + dp[i][j - coins[i-1]]) % 1000000007;
+                }
+            }
+        }
+        return dp[4][n];
+
+    }
+
+    /**
+     * 方法 2 ： 进一步一维 dp ，从状态转移方程可以看出，dp[i][j] 仅仅和 dp[i-1]的状态有关，所以可以压缩为 1 维
+     */
+    public int waysToChange1(int n) {
+        int[] coins = new int[]{1, 5, 10, 25};
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = 1; i <= n; i++) {
+                if (i - coin >= 0) {
+                    dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new WaysToChange().waysToChange1(6));
+    }
+}
