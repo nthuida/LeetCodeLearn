fb5b2e89143aa881f57e5700f3ba893e1ade419c
maohuida <maohuida@kingsoft.com>
2020-05-27 17:06:18 +0800

    5-27

--- src/com/maomao/test/stak/MyStack.java
+++ src/com/maomao/test/stak/MyStack.java
@@ -8,58 +8,59 @@ import java.util.Queue;
  * @author Administrator
  * @date 2019/3/24
  */
-public class MyStack<E> {
-    private  Queue<E> queue1 = new LinkedList<>();
-    private  Queue<E> queue2 = new LinkedList<>();
+public class MyStack {
+    private  Queue<Integer> queue1;
+    private  Queue<Integer> queue2;
+    int top;
 
-    /*
-     * 向队列中执行入栈操作
-     */
-    public void push(E item){
-        queue1.offer(item);
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x){
+        queue1.offer(x);
+        top = x;
     }
 
     /**
      * 1、queue1只有一个元素直接出栈
      * 2、不止一个元素，queue1的元素放入queue2,只剩一个元素，弹出，然后再把queue2的元素放入queue1
      * 出栈
-     * @return
      */
-    public E pop(){
-        E e;
-        if(!isEmpty()){
-            if (queue1.size() == 1) {
-                return queue1.poll();
-            } else {
-                while (queue1.size() > 1) {
-                    queue2.offer(queue1.poll());
-                }
-                e = queue1.poll();
-                while (!queue2.isEmpty()) {
-                    queue1.offer(queue2.poll());
-                }
-            }
+    public int pop(){
+        int e;
+        if (queue1.size() == 1) {
+            return queue1.poll();
         } else {
-            throw new RuntimeException("栈为空，无法执行出栈操作");
+            while (queue1.size() > 1) {
+                queue2.offer(queue1.poll());
+            }
+            e = queue1.poll();
+            while (!queue2.isEmpty()) {
+                int temp = queue2.poll();
+                queue1.offer(temp);
+                top = temp;
+            }
         }
         return e;
     }
 
-    /*
-     * 检查栈是否为空
-     */
+    public int top() {
+        return top;
+    }
+
+
     public  boolean isEmpty(){
-        return queue1.isEmpty()&&queue1.isEmpty();
+        return queue1.isEmpty()&&queue2.isEmpty();
     }
 
     public static void main(String[] args) {
-        MyStack<String> myStack = new MyStack<>();
-        myStack.push("A");
+        MyStack myStack = new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        System.out.println(myStack.top());
         System.out.println(myStack.pop());
-        myStack.push("b");
-        myStack.push("c");
-        System.out.println(myStack.pop());
-        myStack.push("d");
-        System.out.println(myStack.pop());
+        System.out.println(myStack.isEmpty());
     }
 }
