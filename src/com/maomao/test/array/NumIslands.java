8dc2451012cba411a99b2cd870e83e1113e1f1d4
maohuida <maohuida@kingsoft.com>
2020-06-01 16:53:10 +0800

    6-1

--- src/com/maomao/test/array/NumIslands.java
+++ src/com/maomao/test/array/NumIslands.java
@@ -28,6 +28,7 @@ package com.maomao.test.array;
  */
 public class NumIslands {
 
+    private int area = 0;
     public int numIslands(char[][] grid) {
         int count = 0;
         for(int i = 0; i < grid.length; i++) {
@@ -58,6 +59,7 @@ public class NumIslands {
         }
         // 将格子标记为「已遍历过」
         grid[r][c] = '2';
+        area++;
         // 访问上、下、左、右四个相邻结点
         dfs(grid, r - 1, c);
         dfs(grid, r + 1, c);
@@ -76,8 +78,70 @@ public class NumIslands {
         return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
     }
 
+    /**
+     * 求岛屿的最大面积
+     * @param grid
+     * @return
+     */
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1){
+                    dfs(grid, i, j);
+                    if (area >max) {
+                        max = area;
+                    }
+                    area = 0;
+                }
+            }
+        }
+        return max;
+    }
+
+    void dfs(int[][] grid, int r, int c) {
+        // 判断 base case
+        if (!inArea(grid, r, c)) {
+            return;
+        }
+        // 如果这个格子不是岛屿，直接返回
+        if (grid[r][c] != 1) {
+            return;
+        }
+        // 将格子标记为「已遍历过」
+        grid[r][c] = 2;
+        area++;
+        // 访问上、下、左、右四个相邻结点
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    /**
+     * 判断坐标 (r, c) 是否在网格中
+     * @param grid
+     * @param r
+     * @param c
+     * @return
+     */
+    boolean inArea(int[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length && 0 <= c && c < grid[0].length;
+    }
+
     public static void main(String[] args) {
         char[][] grid = {{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
         System.out.println(new NumIslands().numIslands(grid));
+        int[][] a = {
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}
+        };
+        System.out.println(new NumIslands().maxAreaOfIsland(a));
     }
 }
