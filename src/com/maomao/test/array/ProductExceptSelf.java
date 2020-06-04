de9544abccaa4bcd7df7c55363d0524ca104d361
maohuida <maohuida@kingsoft.com>
2020-06-04 16:28:58 +0800

    6-4

---
+++ src/com/maomao/test/array/ProductExceptSelf.java
@@ -0,0 +1,47 @@
+package com.maomao.test.array;
+
+/**
+ * 除自身以外数组的乘积
+ * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 
+ * 中除 nums[i] 之外其余各元素的乘积。
+ *
+ * 示例:
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+ * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ *
+ * @author huida
+ * @date 2020/6/4
+ */
+public class ProductExceptSelf {
+
+    /**
+     * 对于给定索引 ii，等于左边所有数字的乘积乘以右边所有数字的乘积
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        //索引i左边的乘积
+        int[] left = new int[len];
+        //索引i右边的乘积
+        int[] right = new int[len];
+        //最左边没有元素，所以为1
+        left[0] = 1;
+        for (int i=1; i<len; i++) {
+            left[i] = left[i-1] * nums[i-1];
+        }
+        //最右边没有元素，为1
+        right[len-1] = 1;
+        for (int i = len-2; i>=0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        int[] res = new int[len];
+        for (int i=0; i<len; i++) {
+            res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+}
---
+++ src/com/maomao/test/string/GcdOfStrings.java
@@ -0,0 +1,41 @@
+package com.maomao.test.string;
+
+/**
+ * 字符串的最大公因子
+ * 对于字符串 S 和 T，只有在 S = T + ... + T（T 与自身连接 1 次或多次）时，我们才认定 “T 能除尽 S”。
+ * 返回最长字符串 X，要求满足 X 能除尽 str1 且 X 能除尽 str2。
+ *
+ * 示例 1：
+ * 输入：str1 = "ABCABC", str2 = "ABC"
+ * 输出："ABC"
+ *
+ * 示例 2：
+ * 输入：str1 = "ABABAB", str2 = "ABAB"
+ * 输出："AB"
+ *
+ * 示例 3：
+ * 输入：str1 = "LEET", str2 = "CODE"
+ * 输出：""
+ *
+ * @author huida
+ * @date 2020/6/4
+ */
+public class GcdOfStrings {
+
+    public String gcdOfStrings(String str1, String str2) {
+        // 假设str1是N个x，str2是M个x，那么str1+str2肯定是等于str2+str1的。
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        // 辗转相除法求gcd 长度的最大公约数
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+
+    }
+    private int gcd(int a, int b) {
+        if (a % b == 0){
+            return b;
+        }  else {
+           return gcd(b, a%b);
+        }
+    }
+}
