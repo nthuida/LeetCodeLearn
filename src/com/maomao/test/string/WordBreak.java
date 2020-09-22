672df0a83074f4b3a4d1761c90cc512ae3dca320
maohuida <maohuida@kingsoft.com>
2020-09-22 20:32:27 +0800

    9-22

--- src/com/maomao/test/string/WordBreak.java
+++ src/com/maomao/test/string/WordBreak.java
@@ -1,8 +1,6 @@
 package com.maomao.test.string;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 单词拆分
@@ -33,8 +31,9 @@ import java.util.Set;
 public class WordBreak {
 
     /**
-     * 动态规划算法，dp[i]表示s前i个字符能否拆分
+     * 动态规划
      * 转移方程：dp[j] = dp[i] && check(s[i+1, j]);
+     * dp[i]表示s前i个字符能否拆分
      * check(s[i+1, j])就是判断i+1到j这一段字符是否能够拆分
      * 其实，调整遍历顺序，这等价于s[i+1, j]是否是wordDict中的元素
      * 这个举个例子就很容易理解。
@@ -59,5 +58,82 @@ public class WordBreak {
             }
         }
         return dp[s.length()];
+    }
+
+    /**
+     * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，
+     * 使得句子中所有的单词都在词典中。返回所有这些可能的句子。
+     *
+     * 说明：
+     * 分隔时可以重复使用字典中的单词。
+     * 你可以假设字典中没有重复的单词。
+     *
+     * 示例 1：
+     *
+     * 输入:
+     * s = "catsanddog"
+     * wordDict = ["cat", "cats", "and", "sand", "dog"]
+     * 输出:
+     * [
+     *   "cats and dog",
+     *   "cat sand dog"
+     * ]
+     *
+     * 示例 2：
+     *
+     * 输入:
+     * s = "pineapplepenapple"
+     * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+     * 输出:
+     * [
+     *   "pine apple pen apple",
+     *   "pineapple pen apple",
+     *   "pine applepen apple"
+     * ]
+     * 解释: 注意你可以重复使用字典中的单词。
+     *
+     * 示例 3：
+     *
+     * 输入:
+     * s = "catsandog"
+     * wordDict = ["cats", "dog", "sand", "and", "cat"]
+     * 输出: []
+     *
+     * 递归: 先判断当前字符串在不在 wordDict 中，如果在的话就递归的去求剩余字符串的所有组成可能
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public List<String> wordBreakII(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        return wordBreakHelper(s, set, new HashMap<>());
+    }
+
+    private List<String> wordBreakHelper(String s, Set<String> wordDict, Map<String, List<String>> map) {
+        if (s.length() == 0) {
+            return new ArrayList<>();
+        }
+        if (map.containsKey(s)) {
+            //map中有，直接返回
+           return map.get(s);
+        }
+        List<String> res = new ArrayList<>();
+        for (int i=0; i<s.length(); i++) {
+            //字典里包含当前字符串
+            if (wordDict.contains(s.substring(i, s.length()))) {
+                if (i == 0) {
+                    //直接加入
+                    res.add(s.substring(i, s.length()));
+                } else {
+                    //递归得到剩余字符串的结果
+                    List<String> temp = wordBreakHelper(s.substring(0, i), wordDict, map);
+                    for (int k=0; k<temp.size(); k++) {
+                        res.add(temp.get(k) + " " + s.substring(i, s.length()));
+                    }
+                }
+            }
+        }
+        map.put(s, res);
+        return res;
     }
 }
---
+++ src/com/maomao/test/tree/MinCameraCover.java
@@ -0,0 +1,52 @@
+package com.maomao.test.tree;
+
+/**
+ * 监控二叉树
+ * 给定一个二叉树，我们在树的节点上安装摄像头。
+ * 节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+ * 计算监控树的所有节点所需的最小摄像头数量。
+ * @author huida
+ * @date 2020/9/22
+ */
+public class MinCameraCover {
+    int res = 0;
+    /**
+     * @param root
+     * @return
+     */
+    public int minCameraCover(TreeNode root) {
+        if (lrd(root) == 0) {
+            //待覆盖，需要安装一个
+            res++;
+        }
+        return res;
+    }
+
+    /**
+     *   后序遍历
+     *   0 这个节点待覆盖
+     *   1 这个节点已经覆盖
+     *   2 这个节点上安装了相机
+     * @param treeNode
+     * @return
+     */
+    private int lrd(TreeNode treeNode) {
+        if (treeNode == null) {
+            //虚节点，设为已覆盖
+            return 1;
+        }
+        int left = lrd(treeNode.left);
+        int righrt= lrd(treeNode.right);
+
+        if (left == 0|| righrt ==0) {
+            //左或右子树没有覆盖，父节点需要安装一个
+            res++;
+            return 2;
+        } else if (left == 2|| righrt ==2) {
+            //子节点安装了
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
