06451c3160bc2cb5b3d0ec06d4b3dfb0b355e73d
huida <maohuida2009@126.com>
2020-12-16 16:17:55 +0800

    12-15

--- src/com/maomao/test/digit/Generate.java
+++ src/com/maomao/test/digit/Generate.java
@@ -39,4 +39,27 @@ public class Generate {
         }
         return ans;
     }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i <= rowIndex; i++){
+            List<Integer> array = new ArrayList<>();
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    array.add(1);
+                }else{
+                    array.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+                }
+            }
+            ans.add(array);
+        }
+        return ans.get(rowIndex);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Generate().getRow(3));
+    }
+
+
 }
