bacc4eb48ed774c64ed479f3c9fa494571d3df4b
huida <maohuida2009@126.com>
2020-12-17 15:55:00 +0800

    12-17

--- src/com/maomao/test/array/MaxSubArray.java
+++ src/com/maomao/test/array/MaxSubArray.java
@@ -132,6 +132,56 @@ public class MaxSubArray {
         return maxPro;
     }
 
+    /**
+     * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+     * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+     * 返回获得利润的最大值。
+     *
+     * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+     *
+     * 示例 1:
+     *
+     * 输入: prices = [1, 3, 2, 8, 4, 9], fee = 2
+     * 输出: 8
+     * 解释: 能够达到的最大利润:
+     * 在此处买入 prices[0] = 1
+     * 在此处卖出 prices[3] = 8
+     * 在此处买入 prices[4] = 4
+     * 在此处卖出 prices[5] = 9
+     * 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+     *
+     * 动态规划
+     * 定义二维数组 dp[n][2]：
+     *
+     * dp[i][0]表示第 i天不持有可获得的最大利润；
+     * dp[i][1]表示第 i天持有可获得的最大利润。
+     * 定义状态转移方程：
+     *
+     * 不持有：dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+     * 对于今天不持有，可以从两个状态转移过来：1. 昨天也不持有；2. 昨天持有，今天卖出。两者取较大值。
+     *
+     * 持有：dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+     * 对于今天持有，可以从两个状态转移过来：1. 昨天也持有；2. 昨天不持有，今天买入。两者取较大值。
+     *
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public int maxProfit(int[] prices, int fee) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        //初始化 第0天不持有
+        dp[0][0] = 0;
+        //第0天持有
+        dp[0][1] = -prices[0];
+        for(int i=1; i<len; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        }
+        return dp[len-1][0];
+    }
+
+
 
     public static void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
