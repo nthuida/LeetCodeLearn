8de489430fe191af6ed28086fd4ecbaea75d6ae5
huida <maohuida2009@126.com>
2021-11-18 20:17:26 +0800

    11-18

--- src/com/maomao/test/digit/Fib.java
+++ src/com/maomao/test/digit/Fib.java
@@ -101,4 +101,26 @@ public class Fib {
 
     }
 
+    public int climbStairs2(int n) {
+        if (n ==1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i=2; i<n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Fib().climbStairs1(10));
+        System.out.println(new Fib().climbStairs2(10));
+
+    }
+
 }
