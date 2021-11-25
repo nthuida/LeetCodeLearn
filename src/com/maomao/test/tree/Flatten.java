85fc3bf17ca02a561e3967b79e40f861edfaa0d3
huida <maohuida2009@126.com>
2021-11-25 10:03:29 +0800

    11-25

--- src/com/maomao/test/tree/Flatten.java
+++ src/com/maomao/test/tree/Flatten.java
@@ -56,4 +56,38 @@ public class Flatten {
             }
         }
     }
+
+
+    /**
+     * 递归实现
+     * 1、将 root 的左子树和右子树拉平。
+     * 2、将 root 的右子树接到左子树下方，然后将整个左子树作为右子树。
+     * @param root
+     */
+    void flattenII(TreeNode root) {
+        // base case
+        if (root == null) {
+            return;
+        }
+
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+
 }
