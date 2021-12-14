3391da71af3ecce1edcb8731d8cd3e747083f631
huida <maohuida2009@126.com>
2021-12-14 10:52:28 +0800

    12-14

--- src/com/maomao/test/string/Calculate.java
+++ src/com/maomao/test/string/Calculate.java
@@ -74,8 +74,78 @@ public class Calculate {
         return sum;
     }
 
+    /**
+     * 基本计算器
+     * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+     *
+     * 示例 1：
+     * 输入：s = "1 + 1"
+     * 输出：2
+     * 示例 2：
+     * 输入：s = " 2-1 + 2 "
+     * 输出：3
+     * 示例 3：
+     * 输入：s = "(1+(4+5+2)-3)+(6+8)"
+     * 输出：23
+     *
+     * 如果当前是数字，那么更新计算当前数字；
+     * 如果当前是操作符+或者-，那么需要更新计算当前计算的结果 res，并把当前数字 num 设为 0，sign 设为正负，重新开始；
+     * 如果当前是 ( ，那么说明遇到了右边的表达式，而后面的小括号里的内容需要优先计算，所以要把 res，sign 进栈，更新 res 和 sign 为新的开始；
+     * 如果当前是 ) ，那么说明右边的表达式结束，即当前括号里的内容已经计算完毕，所以要把之前的结果出栈，然后计算整个式子的结果；
+     * 最后，当所有数字结束的时候，需要把最后的一个 num 也更新到 res 中。
+     *
+     * @param s
+     * @return
+     */
+    public int calculate1(String s) {
+        Stack<Integer> stack = new Stack<>();
+        //计算结果
+        int res = 0;
+        //当前的数字
+        int num = 0;
+        //运算符
+        int sign = 1;
+        for (int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == ' ') {
+                continue;
+            }
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else if (ch == '+'){
+                res += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (ch == '-') {
+                res += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (ch == '(') {
+                //保存前面计算的结果和运算符
+                stack.push(res);
+                stack.push(sign);
+                //从新开始
+                res = 0;
+                sign = 1;
+            } else if (ch == ')') {
+                //计算当前的结果
+                res += sign * num;
+                num = 0;
+                //运算符
+                res *= stack.pop();
+                //加上前面的结果
+                res += stack.pop();
+            }
+        }
+        //更新最后的结果
+        res += sign * num;
+        return res;
+    }
+
     public static void main(String[] args) {
         String a = "3+2*2";
         System.out.println(new Calculate().calculate(a));
+        String b = "(1+(4+5-2)-3) + (6+2)";
+        System.out.println(new Calculate().calculate1(b));
     }
 }
