27c3ef525756e993b76311f83e64a3cbdeeb8c57
huida <maohuida2009@126.com>
2021-12-16 20:53:21 +0800

    12-16

---
+++ src/com/maomao/test/string/LongestCommonSubsequence.java
@@ -0,0 +1,60 @@
+package com.maomao.test.string;
+
+/**
+ * 最长公共子序列
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ * 示例 1：
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ *
+ * 示例 2：
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ *
+ * 示例 3：
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0 。
+ *
+ * @author: huida
+ * @date: 2021/12/16
+ **/
+public class LongestCommonSubsequence {
+
+    /**
+     * dp[i][j] 表示 text1[0:i], text2[0:j] 的最长公共子序列的长度。i表示text1的前缀长度i
+     * 当i或j为0时，dp[i][j] = 0, 因为空字符串和任何字符串的最长公共子序列的长度都是 0
+     *
+     * 状态转移方程
+     * dp[i][j] =  dp[i−1][j−1]+1,  text1[i-1] = text2[j-1]
+     *             max(dp[i−1][j],dp[i][j−1]), text1[i-1] != text2[j-1]
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                //第一个字符开始
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    // text1[i-1] 和 text2[j-1] 必然在 lcs 中
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    // text1[i-1] 和 text2[j-1] 至少有一个不在 lcs 中
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
---
+++ src/com/maomao/test/string/MinimumDeleteSum.java
@@ -0,0 +1,65 @@
+package com.maomao.test.string;
+
+/**
+ *
+ * 给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
+ * 示例 1:
+ *
+ * 输入: s1 = "sea", s2 = "eat"
+ * 输出: 231
+ * 解释: 在 "sea" 中删除 "s" 并将 "s" 的值(115)加入总和。
+ * 在 "eat" 中删除 "t" 并将 116 加入总和。
+ * 结束时，两个字符串相等，115 + 116 = 231 就是符合条件的最小和。
+ *
+ * 示例 2:
+ *
+ * 输入: s1 = "delete", s2 = "leet"
+ * 输出: 403
+ * 解释: 在 "delete" 中删除 "dee" 字符串变成 "let"，
+ * 将 100[d]+101[e]+101[e] 加入总和。在 "leet" 中删除 "e" 将 101[e] 加入总和。
+ * 结束时，两个字符串都等于 "let"，结果即为 100+101+101+101 = 403 。
+ * 如果改为将两个字符串转换为 "lee" 或 "eet"，我们会得到 433 或 417 的结果，比答案更大。
+ *
+ * @author: huida
+ * @date: 2021/12/16
+ **/
+public class MinimumDeleteSum {
+
+    /**
+     * 转化为 找出两个字符串的最大公共子序列，并且使其ASCII编码最大（因为总编码是一定的，留下的越大删除的越小）
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public int minimumDeleteSum(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        //总ASCII和
+        int sum = 0;
+        for (int i=0; i<n; i++) {
+            sum += s1.charAt(i);
+        }
+        for (int j=0; j<m; j++) {
+            sum += s2.charAt(j);
+        }
+        //ASCII和最大的最长公共子序列
+        int[][] dp = new int[n+1][m+1];
+        for (int i =1; i<=n ;i++) {
+            for (int j=1; j<=m; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    //相等，计算和
+                    dp[i][j] = dp[i-1][j-1] + s1.charAt(i-1);
+                } else {
+                    //不想等
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return sum - dp[n][m]*2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MinimumDeleteSum().minimumDeleteSum("sea", "eat"));
+    }
+}
